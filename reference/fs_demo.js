694b62c948dbdb76b75367ab34b97a7f331f1c36
CodeinX <aseyeged@gmail.com>
2026-01-22 15:07:46 +0100

    Refactor fs_demo.js by commenting out folder creation and file appending code for clarity

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 //Create folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-    if (err)
-        throw err;
-    console.log('Folder created...');
-});
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if (err)
+//         throw err;
+//     console.log('Folder created...');
+//     }
+// );
 
 //Create and write to file
 const filePath = path.join(__dirname, '/test', 'hello.txt');
@@ -16,11 +17,18 @@ fs.writeFile(filePath, 'Hello World!', err => {
     if (err)
         throw err;
     console.log('File written to', filePath);
-});
+
+    fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), '\nnew life!', err => {
+        if (err)
+            throw err;
+        console.log('Text appended to', theTextFile);
+    });
+    }
+);
 
 //Appending to a written file
-fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), '\nnew life!', err => {
-    if (err)
-        throw err;
-    console.log('Text appended to', theTextFile);
-});
+// fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), '\nnew life!', err => {
+//     if (err)
+//         throw err;
+//     console.log('Text appended to', theTextFile);
+// });
