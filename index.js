eef373667f286640dd8c9cbe77ee96627f1c0154
CodeinX <aseyeged@gmail.com>
2026-01-22 23:22:39 +0100

    Implement Logger class and event handling; comment out previous person-related code in index.js and add event_demo.js to demonstrate event emission

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const person = require('./person');
+// const person = require('./person');
 
-console.log(person);
+// console.log(person);
 
-const Person = require('./person');
+// const Person = require('./person');
 
-const person1 = new Person('John Doe', 30);
+// const person1 = new Person('John Doe', 30);
 
-person1.greeting();
+// person1.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener:', data));
+
+logger.log('Hello World');
