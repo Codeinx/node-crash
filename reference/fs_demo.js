604916f2fac9524dca62fe5d2606c0b2bd614645
CodeinX <aseyeged@gmail.com>
2026-01-22 15:33:50 +0100

    Refactor fs_demo.js to comment out file reading code and implement file renaming functionality; add new helloworld.txt file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -34,9 +34,17 @@ const path = require('path');
 // });
 
 //Read file
-fs.readFile(path.join(__dirname, 'text', 'hello.txt'), 'utf8', (err, data) => {
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err)
+//         throw err;
+//     console.log(data);
+//     }
+// );
+
+//Rename file
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), err => {
     if (err)
         throw err;
-    console.log(data);
+    console.log('File renamed to', __dirname);
     }
 );
