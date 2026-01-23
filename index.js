21d92205e0393b9a5c74f1751fdb33f529d8ef31
CodeinX <aseyeged@gmail.com>
2026-01-23 02:13:06 +0100

    Add HTTP server functionality in index.js, create new HTML files for homepage and about page, and update package.json with start and dev scripts

--- index.js
+++ index.js
@@ -8,10 +8,26 @@
 
 // person1.greeting();
 
-const Logger = require('./logger');
+// const Logger = require('./logger');
 
-const logger = new Logger();
+// const logger = new Logger();
 
-logger.on('message', data => console.log('Called Listener:', data));
+// logger.on('message', data => console.log('Called Listener:', data));
 
-logger.log('Hello World');
+// logger.log('Hello World');
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    if(req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Homepage</h1>');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
